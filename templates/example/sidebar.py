a1e444cb4340aa2adf3cfcf56ed13ebc7bb38a70
Johannes Rieke <johannes.rieke@gmail.com>
2020-12-16 22:34:40 +0100

    Refactor templates to contain template + template-specific sidebar components

---
+++ templates/example/sidebar.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def show():
+    """Shows the sidebar components for the template and returns user inputs as dict."""
+    
+    inputs = {}
+    
+    with st.sidebar:
+        st.write("Working on it... ;)")
+        
+    return inputs
+
+
+if __name__ == "__main__":
+    show()
+    
+    
