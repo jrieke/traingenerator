64c685c9680cb4f954bcc93a79bb1fc9a6e53bdd
Johannes Rieke <johannes.rieke@gmail.com>
2020-12-17 04:33:47 +0100

    Remove old code

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -14,18 +14,6 @@ from jinja2 import Environment, FileSystemLoader
 import pytest
 import tempfile
 
-# import shutil
-
-
-# def render_template(**kwargs):
-#     """Renders template (passing in kwargs) and returns Python code."""
-#     env = Environment(
-#         loader=FileSystemLoader("templates"), trim_blocks=True, lstrip_blocks=True,
-#     )
-#     template = env.get_template(f"image_classification_{kwargs['framework']}.py.jinja")
-#     code = template.render(header=lambda x: "", notebook=False, **kwargs)
-#     return code
-
 
 def exec_in_tmp_dir(code):
     """Executes code in temporary working directory."""
@@ -43,9 +31,7 @@ def exec_in_tmp_dir(code):
 class PseudoDirEntry:
     def __init__(self, path):
         self.path = os.path.realpath(path)
-        self.name = os.path.basename(self.path)
-        self.is_dir = os.path.isdir(self.path)
-        self.stat = lambda: os.stat(self.path)
+        self.name = os.path.basename(path)
 
 
 def test_all_templates(subtests, pytestconfig):
@@ -123,59 +109,3 @@ def test_all_templates(subtests, pytestconfig):
 # @pytest.mark.parametrize("inputs", input_combinations)
 # def test_abc(a, inputs):
 #     a ** 2
-
-
-# @pytest.mark.parametrize(
-#     "model_func", ["sklearn.svm.SVC", "sklearn.linear_model.Perceptron"]
-# )
-# @pytest.mark.parametrize("data_format", ["Numpy arrays", "Image files"])
-# @pytest.mark.parametrize("scale_mean_std", [True, False])
-# @pytest.mark.parametrize("visualization_tool", ["Not at all", "Tensorboard"])
-# def test_sklearn_code(
-#     model_func, data_format, scale_mean_std, visualization_tool, tmp_path
-# ):
-#     code = render_template(
-#         framework="scikit-learn",
-#         model_func=model_func,
-#         data_format=data_format,
-#         resize_pixels=28,
-#         crop_pixels=28,
-#         scale_mean_std=scale_mean_std,
-#         visualization_tool=visualization_tool,
-#     )
-#     # if data_format == "Image files":  # copy some test data
-#     #     shutil.copytree(
-#     #         "tests/data/image-classification-data", tmp_path / "path/to/data/dir"
-#     #     )
-#     execute_code(code, tmp_path)
-
-
-# @pytest.mark.parametrize("model_func", ["resnet18"])
-# @pytest.mark.parametrize("data_format", ["Numpy arrays", "Image files"])
-# @pytest.mark.parametrize("gpu", [True, False])  # this will only use GPU if available
-# @pytest.mark.parametrize("checkpoint", [True, False])
-# @pytest.mark.parametrize("visualization_tool", ["Not at all", "Tensorboard"])
-# def test_pytorch_code(
-#     model_func, data_format, gpu, checkpoint, visualization_tool, tmp_path
-# ):
-#     code = render_template(
-#         framework="PyTorch",
-#         model_func=model_func,
-#         pretrained=False,
-#         data_format=data_format,
-#         gpu=gpu,
-#         checkpoint=checkpoint,
-#         loss="CrossEntropyLoss",
-#         optimizer="Adam",
-#         lr=0.001,
-#         batch_size=128,
-#         num_epochs=1,
-#         print_every=1,
-#         visualization_tool=visualization_tool,
-#     )
-#     # if data_format == "Image files":  # copy some test data
-#     #     shutil.copytree(
-#     #         "tests/data/image-classification-data", tmp_path / "path/to/data/dir"
-#     #     )
-#     execute_code(code, tmp_path)
-
