5972674d109e2650073def7176d8c963597575a7
Johannes Rieke <johannes.rieke@gmail.com>
2021-06-23 01:24:49 +0200

    Remove other files

--- heroku-setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
-python scripts/update-index-html.py
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--template",
-        action="store",
-        default=None,
-        help="Name of the template dir to test (default: all)",
-    )
-
